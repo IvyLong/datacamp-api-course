7f619933ad8a25f9984ffb39c06002d0c2b15f27
Ivy L <ivy.long@rea-group.com>
2025-10-28 20:39:08 +1100

    Create user with password

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,20 @@
 -- Database initialization script for PostgreSQL
 -- This script will be automatically executed when the PostgreSQL container starts
 
+-- Ensure the api_user exists with proper permissions
+-- (This is redundant if POSTGRES_USER is set, but ensures consistency)
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'api_user') THEN
+        CREATE USER api_user WITH PASSWORD 'api_password';
+    END IF;
+END
+$$;
+
+-- Grant necessary permissions to api_user
+GRANT ALL PRIVILEGES ON DATABASE api_db TO api_user;
+ALTER USER api_user CREATEDB;
+
 -- Create the thoughts table (Week 3 schema)
 CREATE TABLE IF NOT EXISTS thoughts (
     id SERIAL PRIMARY KEY,
